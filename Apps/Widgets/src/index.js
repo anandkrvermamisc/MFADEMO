258acfdf35e2b3fe2940a3993d0aa51efee1584e
Anand Kumar Verma <anand.kumar.verma@accenture.com>
2023-10-18 09:02:54 +0100

    Import bootstrap in Widget solved the Eager loading for Shared modules issue.

--- Apps/Widgets/src/index.js
+++ Apps/Widgets/src/index.js
@@ -1,7 +1,1 @@
-import App from './App';
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-
-const el = document.querySelector('#_widgetRoot');
-const root = createRoot(el);
-root.render(<App />);
+import('./bootstrap');
